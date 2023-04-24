2e5bfab4295892cf5a4658f9953040a453f3320d
Eduardo Spaki <eduardo@code21.com.br>
2023-04-24 18:38:33 -0300

    bot arm control ui sketch

--- botarm/App.tsx
+++ botarm/App.tsx
@@ -5,34 +5,92 @@ import {  Pressable, StyleSheet, View, Image } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Pressable >
-        <Image
-          source={require('./assets/arrow.png')}
-          style={styles.upArrow}
-        />
-      </Pressable>
-      
-      <View style={{flexDirection: 'row', columnGap: 64}}>
+
+      <View style={styles.container}>
         <Pressable >
-            <Image
-              source={require('./assets/arrow.png')}
-              style={styles.leftArrow}
-            />
-          </Pressable>
-          <Pressable >
           <Image
             source={require('./assets/arrow.png')}
-            style={styles.rightArrow}
+            style={styles.upArrow}
+          />
+        </Pressable>
+        
+        <View style={{flexDirection: 'row', columnGap: 64}}>
+          <Pressable >
+              <Image
+                source={require('./assets/arrow.png')}
+                style={styles.leftArrow}
+              />
+          </Pressable>
+          <Pressable >
+            <Image
+              source={require('./assets/arrow.png')}
+              style={styles.rightArrow}
+            />
+          </Pressable>
+        </View>
+        
+        <Pressable >
+          <Image
+            source={require('./assets/arrow.png')}
+            style={styles.downArrow}
           />
         </Pressable>
       </View>
       
-      <Pressable >
-        <Image
-          source={require('./assets/arrow.png')}
-          style={styles.downArrow}
-        />
-      </Pressable>
+      <View style={styles.container}>
+        <Pressable >
+          <Image
+            source={require('./assets/arrow.png')}
+            style={styles.upArrow}
+          />
+        </Pressable>
+        
+        <View style={{flexDirection: 'row', columnGap: 64}}>
+          <Pressable >
+              <Image
+                source={require('./assets/closed-hand.png')}
+                style={styles.buttonIcon}
+              />
+          </Pressable>
+          <Pressable >
+            <Image
+              source={require('./assets/opened-hand.png')}
+              style={styles.buttonIcon}
+            />
+          </Pressable>
+        </View>
+        
+        <Pressable >
+          <Image
+            source={require('./assets/arrow.png')}
+            style={styles.downArrow}
+          />
+        </Pressable>
+      </View>
+
+      <View style={styles.container}>
+        <View style={{flexDirection: 'row', columnGap: 64}}>
+          <Pressable >
+              <Image
+                source={require('./assets/default.png')}
+                style={styles.buttonIcon}
+              />
+          </Pressable>
+          <Pressable >
+            <Image
+              source={require('./assets/automatic.png')}
+              style={styles.buttonIcon}
+            />
+          </Pressable>
+          <Pressable >
+            <Image
+              source={require('./assets/switch.png')}
+              style={styles.buttonIcon}
+            />
+          </Pressable>
+        </View>
+        
+      </View>
   
       <StatusBar style="auto" />
     </View>
@@ -64,5 +122,9 @@ const styles = StyleSheet.create({
     width: 64,
     height: 64,
     transform: [{rotate: '270deg'}],
+  },
+  buttonIcon: {
+    width: 64,
+    height: 64,
   },
 });
